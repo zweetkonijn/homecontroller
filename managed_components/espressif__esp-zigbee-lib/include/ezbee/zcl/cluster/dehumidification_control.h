9f897a0d6c4bff5c264ab99aa8ec6f7fe9f74bf0
zweetkonijn <57503281+zweetkonijn@users.noreply.github.com>
2026-05-20 21:12:48 +0200

    Delete managed_components/espressif__esp-zigbee-lib directory

--- managed_components/espressif__esp-zigbee-lib/include/ezbee/zcl/cluster/dehumidification_control.h
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#pragma once
-
-#include <ezbee/zcl/cluster/dehumidification_control_desc.h>
